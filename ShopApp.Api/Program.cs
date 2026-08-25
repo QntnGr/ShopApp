f470fb34eb564be91b245c35497d15d0b203581d
Quentin Goeury <qgoeury-ext@goyard.com>
2026-08-25 15:56:20 +0200

    DI and dbcontext

--- ShopApp.Api/Program.cs
+++ ShopApp.Api/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using ShopApp.Infrastructure.Persistence;
+using ShopApp.Application;
+using ShopApp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,18 +11,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Auto-migrate in development (use proper migrations in production!)
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
+//app.UseGlobalExceptionHandler();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
---
+++ ShopApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopApp.Application.Abstractions.Persistence;
+using ShopApp.Infrastructure.Persistence;
+using ShopApp.Infrastructure.Persistence.Repositories;
+
+namespace ShopApp.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // DbContext
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite(
+                configuration.GetConnectionString("DefaultConnection"),
+                sqliteOptions =>
+                {
+                    sqliteOptions.MigrationsAssembly(
+                        typeof(InfrastructureAssemblyMarker).Assembly.FullName);
+                });
+
+        });
+
+        // Repositories
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp =>
+            sp.GetRequiredService<AppDbContext>());
+
+
+        return services;
+    }
+}
