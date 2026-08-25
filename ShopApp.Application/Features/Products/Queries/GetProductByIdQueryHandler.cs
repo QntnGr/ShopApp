f8bcdae9d92bdd741616f1910b1063c354a3c6a0
Quentin Goeury <qgoeury-ext@goyard.com>
2026-08-25 17:42:11 +0200

    query handler

---
+++ ShopApp.Application/Features/Products/Queries/GetProductByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using ShopApp.Application.Abstractions.Persistence;
+using ShopApp.Domain.Common;
+using ShopApp.Domain.Entities;
+
+namespace ShopApp.Application.Features.Products.Queries.GetProductById;
+
+public sealed class GetProductByIdQueryHandler
+    : IRequestHandler<GetProductByIdQuery, Result<Product>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductByIdQueryHandler(
+        IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Result<Product>> Handle(
+        GetProductByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(
+            request.Id,
+            cancellationToken);
+
+        if (product is null)
+        {
+            return Result<Product>.Failure(
+                new Error(
+                    "Product.NotFound",
+                    $"Product with id '{request.Id}' was not found."));
+        }
+
+        return Result<Product>.Success(product);
+    }
+}
